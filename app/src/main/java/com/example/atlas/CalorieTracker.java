1f931f01e581bab8ea6edb63c7d5fa5fdd39190e
Prithvi <prithviwaraich18@gmail.com>
2022-12-01 16:13:55 -0800

    Starting calorie tracker

---
+++ app/src/main/java/com/example/atlas/CalorieTracker.java
@@ -0,0 +1,14 @@
+package com.example.atlas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalorieTracker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calorie_tracker);
+    }
+}
